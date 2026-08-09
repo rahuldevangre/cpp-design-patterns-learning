81c93639b7b1e7d24fb5bf099df6e9390b12b841
GitHub User <git@github.com>
2026-08-09 19:47:17 +0530

    Fix include paths in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
-#include "patterns/Factory.h"
-#include "patterns/Composite.h"
-#include "patterns/Decorator.h"
-#include "patterns/Facade.h"
-#include "patterns/ChainOfResponsibility.h"
-#include "patterns/Command.h"
-#include "patterns/Observer.h"
-#include "patterns/State.h"
-#include "patterns/Strategy.h"
-#include "patterns/TemplateMethod.h"
+#include "Factory.h"
+#include "Composite.h"
+#include "Decorator.h"
+#include "Facade.h"
+#include "ChainOfResponsibility.h"
+#include "Command.h"
+#include "Observer.h"
+#include "State.h"
+#include "Strategy.h"
+#include "TemplateMethod.h"
 
 int main() {
     Factory::run();
